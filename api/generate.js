d224670cf6580991caa017bf3d0dec975038f055
Chris <christopher.palazzo@gmail.com>
2026-03-25 16:46:20 -0700

    fix: use CommonJS export for Vercel function

--- api/generate.js
+++ api/generate.js
@@ -84,7 +84,7 @@ Prompt: "the letter X"
   "duration": 4,
   "suggestedMode": "modern"}`;
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
